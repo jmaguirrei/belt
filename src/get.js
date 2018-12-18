9bbe2c45ce7cc3103266e1ace807f2ec959454db
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-18 09:51:16 -0300

    Initial commit

--- src/get.js
+++ src/get.js
@@ -2,7 +2,10 @@
 export function get(obj, key, defaultValue) {
 
   // Key is number
-  if (typeof key === 'number') return obj[key] || defaultValue;
+  if (typeof key === 'number') {
+    if (obj[key] === undefined) return defaultValue;
+    return obj[key];
+  }
 
   // Key is string
   const splittedKeys = key.split('.');
